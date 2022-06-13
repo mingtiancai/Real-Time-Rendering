4a731d52d6fc604b63af07299e22a7da8ca98511
mingtiancai <2427373908@qq.com>
2022-06-13 23:11:08 +0800

    COMP: set language std

--- xmake.lua
+++ xmake.lua
@@ -5,6 +5,9 @@ set_xmakever("2.5.1")
 
 add_rules("mode.debug", "mode.release")
 
+set_languages("c++17")
+set_languages("c11")
+
 add_requires("boost")
 add_requires("glfw3")
 add_requires("opengl")
